fbb36a96ad7d82d54a372971ba45de77f3bc0175
Vlad Tipukhov <vlad1kk728@ukr.net>
2022-12-20 11:36:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Form registration</title>
+</head>
+
+<body>
+    <!-- authorization form -->
+    <form action="" method="post">
+        <label>Login</label>
+        <input type="text" class="form-control" name="login" id="login" placeholder="Enter login">
+        <label>Password</label>
+        <input type="password" class="form-control" name="pass" id="pass" placeholder="Enter password">
+        <button type="submit" class="btn">Log in</button>
+        <p>Don't have an account yet? <a href="">Sign up now</a></p>
+    </form>
+</body>
+
+</html>
